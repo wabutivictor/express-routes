fc6c89c57add26d11cfcc23b5771d2b33de2bab7
keya victor <wabutivictor@gmail.com>
2022-10-16 20:03:08 +0300

    update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 //basic routing
 app.get('/', (req, res) => {res.send("hello world")})
 
+
+
 //PORT
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> console.log(`server started on http://localhost:${PORT}`));
