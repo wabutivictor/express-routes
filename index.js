0aa57574b0a94b21b531662ed4d4f26822dcdd55
keya victor <wabutivictor@gmail.com>
2022-10-16 19:18:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+//basic routing
+app.get('/', (req, res) => {res.send("hello world")})
+
+//PORT
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> console.log(`server started on http://localhost:${PORT}`));
